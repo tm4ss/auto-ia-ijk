bdca5befe8af2f4ac20abbf2a22ca1a69d76663e
Gregor Wiedemann <g.wiedemann@leibniz-hbi.de>
2021-03-18 07:57:44 +0100

    first lines of R code

---
+++ Hands-on/1_descriptive_stats.R
@@ -0,0 +1,29 @@
+options(stringsAsFactors = F)
+
+# read csv corpus
+
+corona_artikel <- read.csv2("data/articles_coronaberichterstattung_coronabezug_v1.csv", encoding = "UTF-8")
+
+# print 1st article
+corona_artikel[1, ]
+
+# how many articles per media?
+table(corona_artikel$media_name)
+
+# how many articles per media type?
+
+
+# plot number of articles over time (e.g. line plot, or bar plot with days, weeks, or months)
+# - hint: ggplot2 package + geom_bar/geom_line functions
+
+# plot a line plot version of the previous plot with different lines for each media type
+# - hint: use the group or fill paraemeter of the ggplot function
+
+# for which media do we have comments in the corpus
+
+
+# how many comments per media do we have and which is the most commented article?
+
+
+# create a quanteda corpus object, preprocess tokens, and create a dfm
+
