d8e0aa025586e3ed4e5a380149f13db075d5065c
Gregor Wiedemann <g.wiedemann@leibniz-hbi.de>
2021-03-25 12:27:12 +0100

    corpus sepration skeleton

--- Hands-on/3_topics.R
+++ Hands-on/3_topics.R
@@ -69,6 +69,8 @@ K <- 20
 topicModel <- LDA(corona.corpus.tokens.dfm, K, method="Gibbs", 
                   control=list(iter = 500, seed = 1, verbose = 25, alpha = 0.2, delta = 1 / K ))
 
+# load(paste0("data/topicmodel_", K, ".RData"))
+
 # have a look a some of the results (posterior distributions)
 tmResult <- posterior(topicModel)
 
@@ -83,10 +85,11 @@ json <- createJSON(
   vocab = colnames(phi), 
   term.frequency = colSums(corona.corpus.tokens.dfm))
 # Render JSON to html output (convert json data to UTF-8 explicitly, sonst Umlautchaos!)
-serVis(iconv(json, to="UTF-8"), out.dir = paste0("./ldaviz_K", K), open.browser = TRUE)
+serVis(iconv(json, to="UTF-8"), out.dir = paste0("./ldaviz_K", K), open.browser = F)
 
 
-save(topicModel, file = paste0("data/topicmodel_", K, ".RData"))
+# save(topicModel, corona.corpus.tokens.dfm, file = paste0("data/topicmodel_", K, ".RData"))
+
 
 # Tasks:
 # - run the topic model for different K
@@ -95,3 +98,29 @@ save(topicModel, file = paste0("data/topicmodel_", K, ".RData"))
 #   - select and read most representative documents per topic
 # - write topic labels into csv (topic_id,topic_label)
 
+
+
+# get topic order from ldavis
+# topic proportions
+doc.length <- rowSums(corona.corpus.tokens.dfm)
+topic.frequency <- colSums(theta * doc.length)
+topic.proportion <- topic.frequency/sum(topic.frequency)
+o <- order(topic.proportion, decreasing = TRUE)
+
+# show top docs per topic
+top_n <- 10
+top_docs <- data.frame()
+reordered_k <- o
+for (i in 1:K) {
+  k <- reordered_k[i]
+  selected_docs <- order(theta[, k], decreasing = T)[1:top_n]
+  top_docs_per_k <- data.frame(
+    k = i,
+    id = selected_docs,
+    prob = theta[selected_docs, k],
+    titel = corona_artikel$article_title[selected_docs],
+    text = corona_artikel$article_tt[selected_docs]
+  )
+  top_docs <- rbind(top_docs, top_docs_per_k)
+}
+View(top_docs)
---
+++ Hands-on/4_corpus_separation.R
@@ -0,0 +1,64 @@
+options(stringsAsFactors = F)
+# load & install required packages
+pacman::p_load(tidyverse, janitor, quanteda, topicmodels, LDAvis)
+# read csv corpus
+
+corona_artikel <- load(file = "data/corona_artikel.rda")
+
+# load dfm
+load("data/dfm_50_iter_500.RData")
+
+
+# remove empty docs
+sel_idx <- rowSums(corona.corpus.tokens.dfm) > 0
+corona.corpus.tokens.dfm <- corona.corpus.tokens.dfm[sel_idx, ]
+corona_artikel <- corona_artikel[sel_idx, ]
+
+
+# load topic model with 50 K
+load("data/topicmodel_50_iter_500.RData")
+K <- 50
+
+# extract results
+tmResult <- posterior(topicModel)
+phi <- tmResult$terms   # named beta in the tutorial
+theta <- tmResult$topics
+
+
+# ldavis
+json <- createJSON(
+  phi = phi, 
+  theta = theta, 
+  doc.length = rowSums(corona.corpus.tokens.dfm), 
+  vocab = colnames(phi), 
+  term.frequency = colSums(corona.corpus.tokens.dfm))
+# Render JSON to html output (convert json data to UTF-8 explicitly, sonst Umlautchaos!)
+serVis(iconv(json, to="UTF-8"), out.dir = paste0("./ldaviz_K", K), open.browser = T)
+
+# topicnames
+topic_names <- read.csv("data/topicmodel_50_iter_500_labels.tsv", sep = "\t", fileEncoding = "UTF-8")
+
+# get topic order from ldavis
+# topic proportions
+doc.length <- rowSums(corona.corpus.tokens.dfm)
+topic.frequency <- colSums(theta * doc.length)
+topic.proportion <- topic.frequency/sum(topic.frequency)
+o <- order(topic.proportion, decreasing = TRUE)
+
+# reorder topics
+phi <- phi[o, ]
+rownames(phi) <- topic_names$label
+theta <- theta[, o]
+colnames(theta) <- topic_names$label
+
+# Nächste Aufgaben
+# ----------------
+
+# Dokumentselektion
+# A per threshold (z.B. Themenanteil > 30%)
+# B per rank 1 (höchstwahrscheinliches Topic)
+# Zeitreihenvisualisierung
+# - unterschieden sich die Verlaufskurven für A und B?
+# Korrelationsanalysen
+# - Welche Topics laufen parallel / konträr / abwechselnd?
+
